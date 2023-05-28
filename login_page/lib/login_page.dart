071358cfb2402c1699030f820b89357286488e45
B.A Thisara Dilshan <88025945+THISARA005@users.noreply.github.com>
2023-05-28 11:46:42 +0530

    add sign in options and sign in button

--- login_page/lib/login_page.dart
+++ login_page/lib/login_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -18,7 +20,7 @@ class _LoginPageState extends State<LoginPage> {
         children: [
           Container(
               width: w,
-              height: h * 0.3,
+              height: h * 0.25,
               decoration: const BoxDecoration(
                 image: DecorationImage(
                   image: AssetImage('img/loginimg.png'),
@@ -26,6 +28,12 @@ class _LoginPageState extends State<LoginPage> {
                 ),
               )),
           Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20),
+                topRight: Radius.circular(20),
+              ),
+            ),
             margin: const EdgeInsets.only(left: 20, top: 20),
             width: w,
             child:
@@ -39,7 +47,7 @@ class _LoginPageState extends State<LoginPage> {
                 style: TextStyle(fontSize: 20, color: Colors.grey[500]),
               ),
               SizedBox(
-                height: 50,
+                height: 30,
               ),
               Container(
                 decoration: BoxDecoration(
@@ -102,10 +110,10 @@ class _LoginPageState extends State<LoginPage> {
                       borderSide: BorderSide(color: Colors.white, width: 1.0),
                       borderRadius: BorderRadius.circular(30),
                     ),
-                    hintText: "Email",
+                    hintText: "Password",
                     hintStyle: TextStyle(color: Colors.grey[500]),
                     prefixIcon: Icon(
-                      Icons.email,
+                      Icons.password,
                       color: Colors.grey[500],
                     ),
                     border: OutlineInputBorder(
@@ -117,17 +125,74 @@ class _LoginPageState extends State<LoginPage> {
             ]),
           ),
           SizedBox(
-            height: 50,
+            height: 10,
           ),
           Row(
             children: [
               Expanded(child: Container()),
               Text(
                 "Forgot your Password?",
-                style: TextStyle(fontSize: 20, color: Colors.grey[500]),
+                style: TextStyle(fontSize: 15, color: Colors.grey[500]),
               ),
             ],
-          )
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+              width: w * 0.5,
+              height: h * 0.08,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                image: DecorationImage(
+                  image: AssetImage('img/signin.png'),
+                  fit: BoxFit.cover,
+                ),
+              )),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Or login via",
+                style: TextStyle(fontSize: 15, color: Colors.grey[500]),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 20, right: 0, top: 20),
+                width: w * 0.5,
+                height: h * 0.08,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(70),
+                  image: DecorationImage(
+                    image: AssetImage('img/fbicon.png'),
+                    fit: BoxFit.scaleDown,
+                  ),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(
+                  left: 0,
+                  top: 20,
+                  right: 30,
+                ),
+                width: w * 0.2,
+                height: h * 0.05,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  image: DecorationImage(
+                    image: AssetImage('img/gmailicon.png'),
+                    fit: BoxFit.scaleDown,
+                  ),
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
