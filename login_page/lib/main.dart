aa48462090e86792ca474414849c4b519f7f0a88
B.A Thisara Dilshan <88025945+THISARA005@users.noreply.github.com>
2023-06-01 18:21:08 +0530

    succesfully connect the firebase

--- login_page/lib/main.dart
+++ login_page/lib/main.dart
@@ -1,9 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:login_page/login_page.dart';
 import 'package:login_page/signup_page.dart';
 import 'package:login_page/welcome_page.dart';
 
-void main() {
+Future<void> main() async {
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
